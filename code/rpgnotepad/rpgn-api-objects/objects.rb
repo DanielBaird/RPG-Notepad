09ee4e0f665a7564700ae31d755a307436154f1b
Daniel Baird <daniel@danielbaird.com>
2010-03-21 22:37:28 +1000

    Remembered DataMapper and restarted data objects using it.

---
+++ code/rpgnotepad/rpgn-api-objects/objects.rb
@@ -0,0 +1,80 @@
+
+require 'dm-core'
+
+# tell datamapper to log to stdout
+DataMapper::Logger.new($STDOUT, :debug)
+
+# use an in-memory sqlite db for now..
+DataMapper.setup(:default, 'sqlite3::memory:')
+
+# ------------------------------------------------------------------------
+class User
+  
+  include DataMapper::Resource
+  
+end
+# ------------------------------------------------------------------------
+class Campaign
+  
+  include DataMapper::Resource
+  
+  has n, :players
+  has n, :encounters
+  has n, :nonplayercharacters
+  
+end
+# ------------------------------------------------------------------------
+class Player
+  
+  include DataMapper::Resource
+  
+  belongs_to :campaign
+  has n, :playercharacters
+  
+end
+# ------------------------------------------------------------------------
+class Encounter
+  
+  include DataMapper::Resource
+  
+  has n, :playercharacters, :through => Resource
+  
+  belongs_to :campaign
+  has n, :nonplayercharacters
+  
+end
+# ------------------------------------------------------------------------
+class PlayerCharacter
+  
+  include DataMapper::Resource
+  
+  belongs_to :player
+  
+  has n, :encounters, :through => Resource
+  
+  property :id, Serial
+  property :name, String
+  property :abbreviated_name, String
+  
+  def to_s
+    "A Player Character"
+  end
+  
+end
+# ------------------------------------------------------------------------
+class NonPlayerCharacter
+  
+  include DataMapper::Resource
+  
+  belongs_to :encounter
+  
+  property :id, Serial
+  property :name, String
+  property :abbreviated_name, String
+  
+  def to_s
+    "A Player Character"
+  end
+  
+end
+# ------------------------------------------------------------------------
