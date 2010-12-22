3dad7627c34f60d917d7f873b77c40a396bccf54
brimstone4814 <robert.j.pyke@gmail.com>
2010-12-22 22:31:12 +1000

    Added numeric field

--- code/rpgnotepad_mockup/public/js/framework/fields.js
+++ code/rpgnotepad_mockup/public/js/framework/fields.js
@@ -21,15 +21,15 @@ function UniqueId() {
 // Text Field - Constructor
 
 // A text field
-// A text field has a title, a description and a default value.
+// A text field has a title, a description and a value.
 // It can be changed at any time during the actor's turn, and after the actor's time.
-function TextField(title, description, defaultValue) {
+function TextField(title, description, startValue) {
 	this.inheritFrom = UniqueId;
 	this.inheritFrom();
 
 	this.title = title;
 	this.description = description;
-	this.value = defaultValue;
+	this.value = startValue;
 
 } 
 
@@ -37,7 +37,7 @@ function TextField(title, description, defaultValue) {
 
 // Define public methods 
 TextField.prototype.debugDisplay = function() {
-	alert( "TextField[" + this.id + "]: " + this.title + '(' + this.description + ') = ' + this.value );
+	alert( 'TextField[' + this.id + ']: ' + this.title + '(' + this.description + ') = ' + this.value );
 }
 
 // *********************
@@ -46,19 +46,63 @@ TextField.prototype.debugDisplay = function() {
 // TODO
 
 // *********************
-// Incrementing/Decrementing Numeric- Constructor
+// Numeric Field - Constructor
 // e.g. HP
-// TODO
+
+// A numeric field has a title, description, current value, minumum and maximum.
+// The minimum and maximum values are not enforced.
+// They are used purely for display purposes.
+//   e.g. 	In the case of HP in DND: The minimum is 0, and the maximum is the player's maximum health.
+// 		In special cases, the player's health can be buffed above the player's normal maximum.  
+//		Similarly the player can have a health below 0. 
+// The min and max is not enforced, so as to allow for these kind of special cases. 
+// If the current value of this field is below min, or above max, then the display properties of the field
+// will be changed to draw attention the field.
+// A numeric field can be changed at any time during the actor's turn, and after the actor's time.
+function NumericField(title, description, startValue, min, max) {
+	this.inheritFrom = UniqueId;
+	this.inheritFrom();
+
+	this.title = title;
+	this.description = description;
+	this.value = startValue;
+	this.min = min;
+	this.max = max;
+} 
+
+// Numeric Field - Public Methods
+
+// Define public methods 
+// Display the fields properties via a debug alert
+NumericField.prototype.debugDisplay = function() {
+	alert( 'NumericField[' + this.id + ']: ' + this.title + '(' + this.description + ') = ' 
+	+ this.value + ', (min=' + this.min + ', max=' + this.max + ')');
+}
+
+// Increment numeric field (helper method)
+NumericField.prototype.increment = function() {
+	this.value += 1;
+}
+// Decremend numeric field (helper method)
+NumericField.prototype.decrement = function() {
+	this.value -= 1;
+}
 
 // Note:
 // The following code is for testing purposes only
 // TODO: Remove the following code.
 $(document).ready(function() {
-		
-		alert( "DND Example: " );
-		var a = new TextField('HP', 'Hit Points', '0');
-		var b = new TextField('Mood', 'How the actor is feeling', '');
+		alert( 'DND Example: ' );
+		var a = new NumericField('HP', 'Hit Points', 0, 0, 20);
+		var b = new TextField('Mood', 'How the actor is feeling', 'shaken');
 		a.debugDisplay();
 		b.debugDisplay();
+		a.increment();
+		a.debugDisplay();
+		a.increment();
+		a.debugDisplay();
+		a.decrement();
+		a.decrement();
+		a.debugDisplay();
 	}
 );
